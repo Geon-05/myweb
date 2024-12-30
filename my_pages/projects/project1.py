83c2943feb78be6cb0c8a257a8f22d1ca732b9ce
Geon-05 <zqrc05@gmail.com>
2024-12-30 14:34:11 +0900

    path 정리 및 이미지추가가

--- my_pages/projects/project1.py
+++ my_pages/projects/project1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def app():
-    st.title("Project 1")
-    st.write("Details about Project 1.")
+    st.title("이미지 색상화 및 손실 부분 복원 AI 경진대회")
+    st.write("알고리즘 | 월간 데이콘 | 비전 | 이미지 복원 | 이미지 색상화 | SSIM")
     st.image("img/imgpro/damage.png", caption="Project 1 Image")
--- my_pages/projects/project3.py
+++ my_pages/projects/project3.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 def app():
-    st.title("Project 3")
-    st.write("Details about Project 3.")
-    st.image("img/koreaculture/스크린샷 2024-12-23 182041.png", caption="Project 3 Image")
+    st.title("여가생활 추천 챗봇")
+    st.write("Python | Gemini API | Prompt Engineering | Chatbot")
+    st.image("koreaculture/img/0101_main.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0102_menu.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0201_chatbotmain.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0202_loading.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0203_service01.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0204_service02.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0205_service03.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0301_loading.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0302_contant.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0303_rank.png", caption="Project 3 Image")
+    st.image("koreaculture/img/0304_search.png", caption="Project 3 Image")
