644b67a599134fcf038ab3b8fee569cafed613e4
Geon-05 <zqrc05@gmail.com>
2024-12-30 16:20:54 +0900

    통합합

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-from my_pages import home, about, projects_main, contact
+from my_pages import home, projects_main, contact
 
 # 사이드바 메뉴
 st.sidebar.title("Portfolio")
-page = st.sidebar.radio("Go to", ["Home", "About Me", "Projects", "Contact"])
+page = st.sidebar.radio("Go to", ["Home", "Projects", "Contact"])
 
 # 각 페이지 연결
 if page == "Home":
     home.app()
-elif page == "About Me":
-    about.app()
 elif page == "Projects":
     projects_main.app()
 elif page == "Contact":
